7d20d24940847fa7b7db6c963e2289a12dda4d19
hnz989 <zhenghualin2008@gmail.com>
2022-08-09 16:43:08 -0400

    Navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import logo from '../logo.svg'
 
 const Navbar = () => {
-  return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
-  )
+    return (
+        <div>
+            <Link to='/'>
+                <img src={logo} alt='cocktail db logo' className='logo' />
+            </Link>
+            <ul className='nav-links'>
+                <li>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default Navbar
