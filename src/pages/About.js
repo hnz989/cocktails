9ed1cc220a8db05001806fdf25ac1c3d316123c6
hnz989 <zhenghualin2008@gmail.com>
2022-08-11 15:09:13 -0400

    Chang Navbar Style and About Page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,12 +5,9 @@ const About = () => {
         <section className='section about-section'>
             <h1 className='section-title'>about us</h1>
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                software like Aldus PageMaker including versions of Lorem Ipsum.
+                Welcome to TheCocktailDB
+                An open, crowd-sourced database of drinks and cocktails from around the world.
+                We also offer a free JSON API for anyone wanting to use it.
             </p>
         </section>
     )
